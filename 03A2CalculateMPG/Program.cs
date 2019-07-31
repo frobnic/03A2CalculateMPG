7d35d76e3949f05a842938f639e1f8400c77d0e6
frank paulsen <frobnic+ita@gmail.com>
2019-07-31 15:44:48 +0200

    Projektdateien hinzufügen.

---
+++ 03A2CalculateMPG/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03A2CalculateMPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles, gallons, mpg;
+
+            Console.Write("Tankmenge in Liter: ");
+            gallons = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Strecke in km     : ");
+            miles = Convert.ToDouble(Console.ReadLine());
+
+            mpg = ( gallons / miles ) * 100;
+
+            Console.WriteLine("Verbrauch " + mpg);
+
+        }
+    }
+}
